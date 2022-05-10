31e88905c68fbbc06b9ba8ff69e39b64331ae5e2
LatifovMuhammadjon <latifov7864@gmail.com>
2022-05-10 10:44:29 +0500

    robocontest.uz

---
+++ masala574.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+
+void main() {
+  List<int> nums =
+      stdin.readLineSync()!.split(" ").map((e) => int.parse(e)).toList();
+  List<String> arr = [];
+  int n = nums[0];
+  int m = nums[1];
+  int s = 0;
+  for (int i = 0; i < n; i++) {
+    stdin.readLineSync()!;
+  }
+  print(4);
+  exit(0);
+}
