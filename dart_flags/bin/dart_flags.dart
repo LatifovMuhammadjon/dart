6ad274cf5459c7fa6fea4a4ad6bf3a07aa2542f9
LatifovMuhammadjon <latifov7864@gmail.com>
2022-05-26 16:48:36 +0500

    files are modified

--- dart_flags/bin/dart_flags.dart
+++ dart_flags/bin/dart_flags.dart
@@ -4,4 +4,5 @@ void main(List<String> args) async {
   Flag flag = Flag();
   await flag.loadData("https://pokur.su/");
   await flag.loadFlags();
+  await flag.loadCountryNames();
 }
--- dart_flags/bin/flags.dart
+++ dart_flags/bin/flags.dart
@@ -6,6 +6,7 @@ import 'package:http/http.dart';
 class Flag {
   List urlList = [];
   List fileNameList = [];
+  List countryNameList = [];
 
   loadData(url) async {
     var response = await get(Uri.parse(url));
@@ -16,6 +17,7 @@ class Flag {
           .querySelectorAll('.country-currencies-table img[src^="/flags/"]');
       for (var i = 0; i < flags.length; i++) {
         fileNameList.add(flags[i].attributes["src"].toString().substring(1));
+        countryNameList.add(flags[i].attributes["alt"].toString());
         urlList.add(url + fileNameList[i]);
       }
     }
@@ -26,9 +28,16 @@ class Flag {
       var response = await get(Uri.parse(urlList[i]));
       if (response.statusCode == 200) {
         var document = parse(response.body);
-        var file = new File("E:/coding/dart/dart_flags/${fileNameList[i]}").create();
+        var file =
+            new File("E:/coding/dart/dart_flags/${fileNameList[i]}").create();
         file.then((value) => value.writeAsString(response.body));
       }
     }
+  }
+
+  loadCountryNames() {
+    var str = countryNameList.join("\n");
+    var file = new File("E:/coding/dart/dart_flags/country_list.txt").create();
+    file.then((value) => value.writeAsString(str));
   }
 }
